1cbb83b0e8352c43a20653e5de4a5ab8783d9c49
ALOKCS9700 <alok_cs@outlook.com>
2022-05-15 11:01:27 +0530

    a

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,10 @@ router.use('/loggeduser',checkUserAuth)
 
 
 //Public Routes
+router.get('/home',(req, res) => {
+    res.send("welcome")
+})
+
 router.post('/register',UserController.userRegistration)
 router.post('/login',UserController.userLogin)
 router.post('/send-reset-password-email',UserController.sendUserPasswordResetEmail)
